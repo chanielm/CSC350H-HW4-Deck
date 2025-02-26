00a9c14d57726ab23f48f648e5dbe2229000180b
chanielm <chanieljm@gmail.com>
2025-02-25 22:03:03 -0500

    Updated README

--- Program.cs
+++ Program.cs
@@ -7,12 +7,8 @@ deck.Shuffle();
 List<Card> cards = [];
 for (int i = 0; i < 10; i++) cards.Add(deck.TakeTopCard());
 
-Console.WriteLine(cards[0].IsFlipped); // false
-
 // Print the 10 cards collected from the shuffled deck
 foreach (Card card in cards) {
     card.FlipOver();
     Console.WriteLine($"{card.Rank} of {card.Suit}");
 }
-
-Console.WriteLine(cards[0].IsFlipped); // true
---
+++ README.md
@@ -0,0 +1,2 @@
+![alt text](<Screenshot 2025-02-25 220042.png>)
+![alt text](<Screenshot 2025-02-25 220058.png>)
